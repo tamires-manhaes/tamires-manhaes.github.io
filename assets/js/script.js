074730319cc8181c29582aa62bd7361c75a90ec7
Tamires Manhães <40744853+tamires-manhaes@users.noreply.github.com>
2019-09-09 09:26:46 -0300

    deixando animação mais suave

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const contatos = document.querySelector(".contatos");
 
 const animation = new TimelineMax();
 
-animation.fromTo(img , 1.5, {opacity: "0"}, {opacity: "1"}, Power2.easeInOut);
+animation.fromTo(img , 1.7, {opacity: "0"}, {opacity: "1"}, Power2.easeIn);
 animation.fromTo(titulo, 2, {x: "-50%"}, {x: "0%"}, Power2.easeInOut);
 animation.fromTo(subtitulo, 2,{x: "-50%"}, {x: "0%"}, Power2.easeInOut);
 animation.fromTo(contatos, 2, {x: "-50%"}, {x: "0%"}, Power2.easeInOut);
