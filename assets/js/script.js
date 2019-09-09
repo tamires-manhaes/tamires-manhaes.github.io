15411b2aaffbb55df79d4e9fc4ae668b556e8427
Tamires Manhães <40744853+tamires-manhaes@users.noreply.github.com>
2019-09-09 09:27:46 -0300

    deixando animação mais suave

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ const animation = new TimelineMax();
 
 animation.fromTo(img , 1.7, {opacity: "0"}, {opacity: "1"}, Power2.easeIn);
 animation.fromTo(titulo, 2, {x: "-50%"}, {x: "0%"}, Power2.easeInOut);
-animation.fromTo(subtitulo, 2,{x: "-50%"}, {x: "0%"}, Power2.easeInOut);
+animation.fromTo(subtitulo, 2,{x: "50%"}, {x: "0%"}, Power2.easeInOut);
 animation.fromTo(contatos, 2, {x: "-50%"}, {x: "0%"}, Power2.easeInOut);
 
 
