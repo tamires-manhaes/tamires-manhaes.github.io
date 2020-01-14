ca2cdcd280680a84c51287cd3a5a08e205d1cf47
Tamires <tamiresmanhaes4@gmail.com>
2020-01-13 22:17:02 -0300

    Subindo nova versao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 const img = document.querySelector("#img");
 const titulo = document.querySelector(".titulo");
 const subtitulo = document.querySelector(".subtitulo");
-const contatos = document.querySelector(".contatos");
+const contatos = document.querySelector(".nav-hamburguer");
 
+console.log(contatos);
 const animation = new TimelineMax();
 
 animation.fromTo(img , 1.7, {opacity: "0"}, {opacity: "1"}, Power2.easeIn);
-animation.fromTo(titulo, 2, {x: "-50%"}, {x: "0%"}, Power2.easeInOut);
-animation.fromTo(subtitulo, 2,{x: "50%"}, {x: "0%"}, Power2.easeInOut);
-animation.fromTo(contatos, 2, {x: "-50%"}, {x: "0%"}, Power2.easeInOut);
+animation.fromTo(titulo, 2, {opacity: "0"}, {opacity: "1"}, Power2.easeInOut);
+animation.fromTo(subtitulo, 2,{opacity: "0"}, {opacity: "1"}, Power2.easeInOut);
+animation.fromTo(contatos, 2, {opacity: "0"}, {opacity: "1"}, Power2.easeInOut);
 
 $(document).ready(function(){
     $("#hero").click(function() {
