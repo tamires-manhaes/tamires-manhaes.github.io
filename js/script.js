912fafdd677184d4338f4033379d2bf813fc0176
Tamires <tamires.manhaes@cubos.io>
2020-09-16 19:56:24 -0300

    adicionando animação do scroll e apagando animações antigas

--- js/script.js
+++ js/script.js
@@ -1,32 +1,15 @@
-const home = document.querySelector('.home');
-const menu = document.querySelector('.menu-top');
-const sobreMim = document.querySelector('.sobre-mim');
-const experiencias = document.querySelector('.experiencias');
-const conhecimentos = document.querySelector('.conhecimentos');
-const habilidades = document.querySelector('.habilidades'); 
+const body = document.querySelector('body');
 
 const animation = new TimelineMax();
 
-animation.fromTo(menu , 3, {opacity: "0"}, {opacity: "1"}, Power2.easeIn);
-animation.fromTo(home , 3, {opacity: "0"}, {opacity: "1"}, Power2.easeIn);
-animation.fromTo(sobreMim , 4, {opacity: "0"}, {opacity: "1"}, Power2.easeIn);
-animation.fromTo(experiencias , 3, {opacity: "0"}, {opacity: "1"}, Power2.easeIn);
-animation.fromTo(conhecimentos , 3, {opacity: "0"}, {opacity: "1"}, Power2.easeIn);
-animation.fromTo(habilidades , 3, {opacity: "0"}, {opacity: "1"}, Power2.easeIn);
+animation.fromTo(body , 2, {opacity: "0"}, {opacity: "1"}, Power2.easeIn);
 
 
 (function( $ ){
     $( document ).ready(function() {
         $('#hero').click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 600);
+          $('html, body').animate({scrollTop : 0},1500,)
         });
-
-        // $( 'scroll' ).click(function() {
-        //     // $("html, body").animate({ scrollTop: 0 }, 600);
-        //     $('html, body').animate({
-        //       scrollTop: $(hash).offset().top
-        //     }, 800)
-        // });
     });
     
     var date = new Date();
