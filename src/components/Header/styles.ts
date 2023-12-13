a68e98d7596bc3b9e4956e77ecaa7995efe0dbb9
Tamires Manhaes <tamiresmanhaes4@gmail.com>
2023-12-13 17:51:38 -0300

    fixing header spacings

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,6 +5,7 @@ export const Header = styled.header``;
 export const Wrapper = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
 `;
 
 export const Logo = styled.span`
