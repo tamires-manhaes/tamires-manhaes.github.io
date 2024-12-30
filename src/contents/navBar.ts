52a823464476090fb283cbed5508d51878baed06
Tamires <tamiresmanhaes4gmail.com>
2024-12-30 16:49:47 -0300

    feat: Add About Me, Blog Posts, Header, Tech Stack, Pepper Studio, and Projects content

---
+++ src/contents/navBar.ts
@@ -0,0 +1,18 @@
+import { routes } from "@/configs/routes";
+
+export const navbar = {
+  links: [
+    {
+      name: "Home",
+      url: routes.home,
+    },
+    {
+      name: "Blog",
+      url: routes.blog,
+    },
+    {
+      name: "Projects",
+      url: routes.project,
+    },
+  ],
+};
