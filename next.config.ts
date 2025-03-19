72623512c7b910c8cf074ce87c0c62453d22ea17
Tamires <tamiresmanhaes4@gmail.com>
2025-03-19 16:11:37 -0300

    feat: Configure remote image patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'github.com' },
+      { hostname: 'avatars.githubusercontent.com' },
+    ],
+  },
+};
 
 export default nextConfig;
