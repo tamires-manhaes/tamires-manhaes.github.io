e741a0c8764d416cf2fca338483ff05fa16b064a
Tamires <tamiresmanhaes4@gmail.com>
2025-03-26 13:35:17 -0300

    feat: Hide logo link on smaller screens for improved responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ import { illustrations } from '@/configs/illustrations';
 export default function Header() {
   return (
     <header className=" max-w-[1300px] w-full mx-auto px-8 py-2 flex content-center justify-between bg-grey-default text-gray-900 dark:text-textColor shadow-custom">
-      <Link href="/" className="text-xl font-bold">
+      <Link href="/" className="text-xl font-bold hidden md:block">
         <Image src={illustrations.logo} width={200} height={50} alt="logo" />
       </Link>
       <NavBar />
