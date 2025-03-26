a38c3e51784bd5db29226465dac982d520426e72
Tamires <tamiresmanhaes4@gmail.com>
2025-03-26 13:41:38 -0300

    fix: Ensure supabase URL and anon key are defined to prevent runtime errors

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { envs } from '@/configs/envs';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = envs.supabase.supabaseURL;
-const supabaseAnonKey = envs.supabase.supabaseAnonKey;
+const supabaseUrl = envs.supabase.supabaseURL || '';
+const supabaseAnonKey = envs.supabase.supabaseAnonKey || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
